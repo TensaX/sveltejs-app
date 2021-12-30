197107b05066f994696038a6688f3bb8499058ff
TensaX <96866563+TensaX@users.noreply.github.com>
2021-12-30 15:15:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyA_kOxKaHXW17nqkQxafUUdWOyvs88k62M",
-    authDomain: "test-project-e15c2.firebaseapp.com",
-    projectId: "test-project-e15c2",
-    storageBucket: "test-project-e15c2.appspot.com",
-    messagingSenderId: "299203211963",
-    appId: "1:299203211963:web:28d84743d0d26d28442f9e"
+/*Your Firebase config*/
   };
 
 export default firebaseConfig;
